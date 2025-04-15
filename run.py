7dc4623cb2d3fcce10c8ec0905791a47bfc61b2c
Sarah Emilie <151790608+Velmist@users.noreply.github.com>
2025-04-15 13:25:42 -0400

    Update run.py for hosting

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from app import app
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "¡Hola, mundo!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
