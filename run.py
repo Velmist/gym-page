de0e2dd133cd7a0d72fecca9593d66e2b1b34848
Sarah Emilie <151790608+Velmist@users.noreply.github.com>
2025-04-15 13:37:59 -0400

    More problem with host

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from app import app
 import os
 
 @app.route('/')
 def home():
-    return "Hola"
+    return "¡Hola, mundo!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000)) 
+    port = int(os.environ.get("PORT", 5000)) #puerto para el host
     app.run(host="0.0.0.0", port=port)
